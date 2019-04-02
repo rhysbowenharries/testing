34325f7e7fd9823d7c74f265b67a974c81df436e
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-04-02 15:06:29 +0100

    Fourth and fifth pass test

--- specs/my_functions_spec.rb
+++ specs/my_functions_spec.rb
@@ -18,17 +18,17 @@ class FunctionsTest < MiniTest::Test
     add_result = add( 1, 2 )
     assert_equal( 3, add_result )
   end
-  
-  # def test_subtract()
-  #   subtract_result = subtract( 10, 5 )
-  #   assert_equal( 5, subtract_result )
-  # end
-  #
-  # def test_multiply()
-  #   multiply_result = multiply( 4, 2 )
-  #   assert_equal( 8, multiply_result )
-  # end
-  #
+
+  def test_subtract()
+    subtract_result = subtract( 10, 5 )
+    assert_equal( 5, subtract_result )
+  end
+
+  def test_multiply()
+    multiply_result = multiply( 4, 2 )
+    assert_equal( 8, multiply_result )
+  end
+
   # def test_divide()
   #   divide_result = divide( 10, 2 )
   #   assert_equal( 5, divide_result )
