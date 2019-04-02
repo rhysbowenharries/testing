ce640dc75432f5e8ec1fe59f232fb80578baa827
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-04-02 15:16:30 +0100

    8th & 9th test done

--- my_functions.rb
+++ my_functions.rb
@@ -25,3 +25,11 @@ end
 def length_of_string(string)
   return string.length
 end
+
+def join_string(string_1, string_2)
+  return "#{string_1}#{string_2}"
+end
+
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
